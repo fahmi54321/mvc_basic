fd7ccccfe9226f45c4bc2c68db18323f3a1e2db9
fahmiabdulaziz44 <fahmiabdulaziz44@gmail.com>
2025-10-29 21:39:55 +0700

    Extraction of BaseViewMvc Abstract Class

--- app/src/main/java/com/example/mvc/screens/questionslist/QuestionsListItemMvcImpl.java
+++ app/src/main/java/com/example/mvc/screens/questionslist/QuestionsListItemMvcImpl.java
@@ -1,26 +1,23 @@
 package com.example.mvc.screens.questionslist;
 
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.mvc.R;
 import com.example.mvc.questions.Question;
+import com.example.mvc.screens.common.BaseViewMvc;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class QuestionsListItemMvcImpl implements QuestionsListItemViewMvc {
-
-    View mRootView;
-
+public class QuestionsListItemMvcImpl extends BaseViewMvc implements QuestionsListItemViewMvc {
     private final List<Listener> mListeners = new ArrayList<>(1);
     private Question mQuestion;
     private TextView mTxtTitle;
 
     public QuestionsListItemMvcImpl(LayoutInflater layoutInflater, ViewGroup parent) {
-        mRootView = layoutInflater.inflate(R.layout.layout_question_list_item, parent, false);
+        setRootView(layoutInflater.inflate(R.layout.layout_question_list_item, parent, false));
         mTxtTitle = findViewById(R.id.txt_title);
 
         getRootView().setOnClickListener(v -> {
@@ -28,10 +25,6 @@ public class QuestionsListItemMvcImpl implements QuestionsListItemViewMvc {
                 listener.onQuestionClicked(mQuestion);
             }
         });
-    }
-
-    private <T extends View> T findViewById(int id) {
-        return getRootView().findViewById(id);
     }
 
     @Override
@@ -48,10 +41,5 @@ public class QuestionsListItemMvcImpl implements QuestionsListItemViewMvc {
     public void bindQuestion(Question question) {
         mQuestion = question;
         mTxtTitle.setText(question.getTitle());
-    }
-
-    @Override
-    public View getRootView() {
-        return mRootView;
     }
 }
--- app/src/main/java/com/example/mvc/screens/questionslist/listview/QuestionsListViewMvcImpl.java
+++ app/src/main/java/com/example/mvc/screens/questionslist/listview/QuestionsListViewMvcImpl.java
@@ -2,26 +2,25 @@ package com.example.mvc.screens.questionslist.listview;
 
 import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.example.mvc.R;
 import com.example.mvc.questions.Question;
+import com.example.mvc.screens.common.BaseViewMvc;
 import com.example.mvc.screens.questionslist.QuestionsListViewMvc;
 import com.example.mvc.screens.questionslist.adapter.listview.QuestionsListViewAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class QuestionsListViewMvcImpl implements QuestionsListViewAdapter.OnQuestionClickListener, QuestionsListViewMvc {
+public class QuestionsListViewMvcImpl extends BaseViewMvc implements QuestionsListViewAdapter.OnQuestionClickListener, QuestionsListViewMvc {
 
     private final List<Listener> mListeners = new ArrayList<>(1);
-    private final View rootView;
     private final ListView mLstQuestions;
     private final QuestionsListViewAdapter mQuestionsListViewAdapter;
     public QuestionsListViewMvcImpl(LayoutInflater layoutInflater, ViewGroup viewGroup) {
-        rootView = layoutInflater.inflate(R.layout.layout_questions_list,viewGroup, false);
+        setRootView(layoutInflater.inflate(R.layout.layout_questions_list,viewGroup, false));
         mLstQuestions = findViewById(R.id.lst_questions);
         mQuestionsListViewAdapter = new QuestionsListViewAdapter(getContext(), this);
         mLstQuestions.setAdapter(mQuestionsListViewAdapter);
@@ -34,19 +33,6 @@ public class QuestionsListViewMvcImpl implements QuestionsListViewAdapter.OnQues
     @Override
     public void unregisterListener(Listener listener){
         mListeners.remove(listener);
-    }
-
-    private Context getContext() {
-        return rootView.getContext();
-    }
-
-    private <T extends View> T findViewById(int id) {
-        return rootView.findViewById(id);
-    }
-
-    @Override
-    public View getRootView() {
-        return rootView;
     }
 
     @Override
