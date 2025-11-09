168e1708fe877c59f15564d4ba85a69c0c549dc7
fahmiabdulaziz44 <fahmiabdulaziz44@gmail.com>
2025-11-09 20:14:58 +0700

    add view group

--- app/src/main/java/com/example/mvc/screens/questionslist/listview/QuestionsListFragment.java
+++ app/src/main/java/com/example/mvc/screens/questionslist/listview/QuestionsListFragment.java
@@ -19,7 +19,7 @@ public class QuestionsListFragment extends BaseFragment implements BackPressedLi
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        QuestionsListViewMvc viewMvc = getCompositionRoot().getViewMvcFactory().getQuestionsListViewMvc(null);
+        QuestionsListViewMvc viewMvc = getCompositionRoot().getViewMvcFactory().getQuestionsListViewMvc(container);
         questionsListController = getCompositionRoot().getQuestionsListController();
         questionsListController.bindView(viewMvc);
         return viewMvc.getRootView();
--- app/src/main/java/com/example/mvc/screens/questionslist/recycler/QuestionsRecyclerListFragment.java
+++ app/src/main/java/com/example/mvc/screens/questionslist/recycler/QuestionsRecyclerListFragment.java
@@ -19,7 +19,7 @@ public class QuestionsRecyclerListFragment extends BaseFragment implements BackP
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        QuestionsListViewMvc viewMvc = getCompositionRoot().getViewMvcFactory().getQuestionsListViewMvc(null);
+        QuestionsListViewMvc viewMvc = getCompositionRoot().getViewMvcFactory().getQuestionsListViewMvc(container);
         questionsListController = getCompositionRoot().getQuestionsListController();
         questionsListController.bindView(viewMvc);
         return viewMvc.getRootView();
